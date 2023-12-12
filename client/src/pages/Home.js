86267c49fcc835d858391b4ee02a691e54810a17
Sabdulgafar <salawu.itopa1998@gmail.com>
2023-12-12 02:16:44 +0100

    Editting the homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,26 @@
 import React from "react";
 import '../App.css';
 // import Navbar from "../components/Navbar";
-// import avatar from "../components/imgs/avatar.png";
+import banna from "../components/imgs/banna.jpg";
 
 export const Home = ()=>{
     return(
-        <div className="container">
-            <h1>Welcome to Ize Store App</h1>
+        <div >
+            <div className="container" style={{backgroundImage:`url(${banna})`,
+                                            backgroundSize: "cover",
+                                            backgroundRepeat: "no-repeat",
+                                            width: "300px",
+                                            height: "300px",
+                                            borderTopRightRadius: "30px",
+                                            borderBottomRightRadius: "30px",
+                                            borderBottomLeftRadius: "30px",
+                                            marginTop: "1.75rem",
+                                            filter: "blur(2px)",
+                                            WebkitBackdropFilter: "blur(2px)",
+            }}></div>
+            <div className="bg-text">
+                <h1>Hello world</h1>
+            </div>
         </div>
     );
 }

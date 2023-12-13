bdbe222a4edb9a1c6a245142be464c7e5ee26375
Sabdulgafar <salawu.itopa1998@gmail.com>
2023-12-13 03:26:17 +0100

    necessary dashboard components created

---
+++ client/src/components/left-side-bar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../App.css';
+
+const leftSideBar = ()=>{
+    return(
+        <div>
+        </div>
+    );
+};
+
+export default leftSideBar;
---
+++ client/src/components/sales.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../App.css';
+
+const Sales = ()=>{
+    return(
+        <div>
+        </div>
+    );
+};
+
+export default Sales;

2dcefc36d1c912821c5280da4138b2e1ef191100
Sabdulgafar <salawu.itopa1998@gmail.com>
2023-12-14 13:47:48 +0100

    adding extra dashboard features

--- client/src/components/inventory.js
+++ client/src/components/inventory.js
@@ -3,7 +3,24 @@ import '../App.css';
 
 export const Inventory = ()=>{
     return(
-        <div>
+        <div className='inventory-section'>
+            <div>
+                <h1 className='inventory-title'>Inventory Management</h1>
+                <div className='inventory-overview'>
+                    <div>
+                        <a href='!'>Total Products: <span id="">10</span></a>   
+                    </div>
+                    <div>
+                        <a href='!' style={{color:'orange'}}>Low Stock: <span id="">12</span></a>   
+                    </div>
+                    <div>
+                        <a href='!' style={{color:'red'}}>Out of Stuck: <span id="">3</span></a>   
+                    </div>
+                    <div>
+                        <a href='!'>Supplies: <span id="">7</span></a>   
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };

74699d48eee6402efb70a5643d1dab519ee88bfc
Sabdulgafar <salawu.itopa1998@gmail.com>
2023-12-21 08:38:26 +0100

    setting two elements in admindb

--- client/src/components/transact.js
+++ client/src/components/transact.js
@@ -4,6 +4,15 @@ import '../App.css';
 export const Transact = ()=>{
     return(
         <div>
+            <div className="Transaction-section">
+                <div className='transaction-1'>
+                    <h3>Transaction</h3>
+                </div>
+                <div className='minirecord-1'>
+                    <h3>Mini Record</h3>
+                    <p></p>
+                </div>
+            </div>
         </div>
     );
 };
